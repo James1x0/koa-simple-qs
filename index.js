a72f2cb4dc57d94f6271e85b921432a6c808bccc
James-Mac <james@aehr.org>
2016-10-17 14:34:51 -0600

    Only parse GET requests

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const qs = require('qs');
 
 module.exports = ( options = { plainObjects: true } ) => {
   return function*(next) {
-    this.request.body = qs.parse(this.request.querystring, options);
+    if ( this.request.method === 'GET' ) {
+      this.request.body = qs.parse(this.request.querystring, options);
+    }
+
     yield next;
   };
 };
