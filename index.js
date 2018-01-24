9c9207a081ab0a6247f9e8a00d9a507b37caffff
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2018-01-24 18:48:33 -0500

    Adding set functionality

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const qs = require('qs');
 module.exports = ( options = { plainObjects: true } ) => {
   return async (next) {
     if ( this.request.method === 'GET' ) {
-      this.request.body = qs.parse(this.request.querystring, options);
+      Object.assign(this.request, {
+      	get query() { return qs.parse(this.request.querystring, options) }
+      	set query(obj) { this.request.querystring = qs.stringify(obj) }
+      }
     }
 
     await next;
