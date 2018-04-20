23b3d0bb56cd41fae43b869b5968bdbe15a6a086
James <james@aehr.org>
2018-04-20 11:12:43 -0600

    Async changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const qs = require('qs');
 
-module.exports = ( options = { plainObjects: true } ) => {
-  return function*(next) {
-    if ( this.request.method === 'GET' ) {
+module.exports = (options = { plainObjects: true }) => {
+  return async (next) => {
+    if (this.request.method === 'GET') {
       this.request.body = qs.parse(this.request.querystring, options);
     }
 
-    yield next;
+    await next;
   };
 };
